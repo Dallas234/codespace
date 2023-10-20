e070f3aa257136337154a8af996c4cdb72e02842
Dallas234 <148400889+Dallas234@users.noreply.github.com>
2023-10-20 19:51:16 +0000

    Fri, Oct 20, 2023, 7:51 PM +00:00

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 
 # ask user for name
+
 name = input("Whats's your name? " )
+
 #remove whitespace from string
 name = name.strip()
+
+#Capitalize users name
+name = name.capitalize()
+
 #print users name
-print(f"Hello,  {name}")
+print(f"Hello, {name}")
